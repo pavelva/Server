a4a8bffc4a8168bdcc1f7bd3e91911525697b518
pavel <pavelva@hotmail.com>
2014-09-07 07:11:38 +0300

    1345789

---
+++ webTest/default1.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using website;
+namespace webTest
+{
+    public partial class default1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lbl.Text = MyServer.message;
+        }
+    }
+}
