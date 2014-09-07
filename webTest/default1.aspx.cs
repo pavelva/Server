c1f001c09505638c290fcbff8cadd3f7800479e6
pavel <pavelva@hotmail.com>
2014-09-07 07:25:28 +0300

    sad

--- webTest/default1.aspx.cs
+++ webTest/default1.aspx.cs
@@ -4,14 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using website;
+
 namespace webTest
 {
     public partial class default1 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            lbl.Text = MyServer.message;
+            
         }
     }
 }
