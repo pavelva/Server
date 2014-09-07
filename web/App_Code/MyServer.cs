c216098d4ce086495da7d3bb3df05317427cee99
pavel <pavelva@hotmail.com>
2014-09-07 05:34:24 +0300

    a

--- web/App_Code/MyServer.cs
+++ web/App_Code/MyServer.cs
@@ -14,59 +14,59 @@ namespace website
 {
     public class MyServer
     {
-        public static string message = getMyIP();
-        public static bool running = false;
+        //public static string message = getMyIP();
+        //public static bool running = false;
 
-        public static void run()
-        {
-            if(!running)
-                ThreadPool.QueueUserWorkItem(start);
-        }
-        public static void start(Object o)
-        {
-            while (true)
-            {
-                //TcpListener server = new TcpListener(IPAddress.Parse(getMyIP()), 1234);
-                //server.Start();
-                //running = true;
-                //TcpClient client = server.AcceptTcpClient();
-                //message = getRequest(client);
-            }
-        }
+        //public static void run()
+        //{
+        //    if(!running)
+        //        ThreadPool.QueueUserWorkItem(start);
+        //}
+        //public static void start(Object o)
+        //{
+        //    while (true)
+        //    {
+        //        //TcpListener server = new TcpListener(IPAddress.Parse(getMyIP()), 1234);
+        //        //server.Start();
+        //        //running = true;
+        //        //TcpClient client = server.AcceptTcpClient();
+        //        //message = getRequest(client);
+        //    }
+        //}
 
-        private static string getRequest(TcpClient client)
-        {
-            NetworkStream stream = client.GetStream();
+        //private static string getRequest(TcpClient client)
+        //{
+        //    NetworkStream stream = client.GetStream();
 
-            StringBuilder data = new StringBuilder();
+        //    StringBuilder data = new StringBuilder();
 
-            while (!stream.DataAvailable) ;
+        //    while (!stream.DataAvailable) ;
 
-            Byte[] bytes = new Byte[client.Available];
+        //    Byte[] bytes = new Byte[client.Available];
 
-            stream.Read(bytes, 0, bytes.Length);
+        //    stream.Read(bytes, 0, bytes.Length);
 
-            data.Append(Encoding.UTF8.GetString(bytes));
-            //console.writeLine("Client Request String:\n" + data.ToString());
-            //console.writeLine("Client Address: " + ((IPEndPoint)client.Client.RemoteEndPoint).Address.ToString());
-            return data.ToString();
-        }
+        //    data.Append(Encoding.UTF8.GetString(bytes));
+        //    //console.writeLine("Client Request String:\n" + data.ToString());
+        //    //console.writeLine("Client Address: " + ((IPEndPoint)client.Client.RemoteEndPoint).Address.ToString());
+        //    return data.ToString();
+        //}
 
-        private static string getMyIP()
-        {
-            IPHostEntry host;
-            string localIP = "?";
-            host = Dns.GetHostEntry(Dns.GetHostName());
-            foreach (IPAddress ip in host.AddressList)
-            {
-                if (ip.AddressFamily.ToString() == "InterNetwork")
-                {
-                    localIP = ip.ToString();
-                }
-            }
-            Console.WriteLine(localIP);
+        //private static string getMyIP()
+        //{
+        //    IPHostEntry host;
+        //    string localIP = "?";
+        //    host = Dns.GetHostEntry(Dns.GetHostName());
+        //    foreach (IPAddress ip in host.AddressList)
+        //    {
+        //        if (ip.AddressFamily.ToString() == "InterNetwork")
+        //        {
+        //            localIP = ip.ToString();
+        //        }
+        //    }
+        //    Console.WriteLine(localIP);
 
-            return localIP;
-        }
+        //    return localIP;
+        //}
     }
 }
